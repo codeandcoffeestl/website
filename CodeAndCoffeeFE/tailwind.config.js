19617f4b958ad3641edf9bccfc2231f6f9c182a7
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-02-20 19:12:10 -0600

    added custom template, configured tailwind + primeReact

---
+++ CodeAndCoffeeFE/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        PrimaryColor: "#4B2E17",
+        SecondaryColor: "#17A2B8",
+        WAccentColor: "#FAF9F6",
+        GAccentColor: "#D3D3D3",
+        OHighlightColor: "#FFA500",
+        YHighlightColor: "#FFD700",
+      },
+    },
+  },
+  plugins: [],
+};
