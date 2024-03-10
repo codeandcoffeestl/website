aa8b183419a48e6124623599c89e7b0a0fafe40c
itsHackinTime <109614480+itsHackinTime@users.noreply.github.com>
2024-03-10 12:57:28 -0500

    added banner to Hero.tsx

--- client/src/components/home/HeroC.tsx
+++ client/src/components/home/HeroC.tsx
@@ -1,6 +1,26 @@
+import { Button } from "primereact/button"
+
 const HeroC = () => {
     return (
-        <div>HeroC</div>
+
+        <div>
+            <div>
+                <p>
+                    A Meetup community for developers to grow and make friends  - St Louis
+                </p>
+            </div>
+            <div>
+                <p>
+                    A central hub for digital space in the city
+                </p>
+            </div>
+            <Button>
+                Our Sponsors
+            </Button>
+            <Button>
+                Sign Up for Volunterer 
+            </Button>
+        </div>
     )
 }
 
