d79a6569eb1ae3935da2cf1fc202a3c205e88c21
itsHackinTime <109614480+itsHackinTime@users.noreply.github.com>
2024-03-10 12:50:53 -0500

    basic layout for banner added

---
+++ client/src/components/home/Banner.tsx
@@ -0,0 +1,26 @@
+import { Button } from "primereact/button"
+
+const BannerC = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                    A Meetup community for developers to grow and make friends  - St Louis
+                </p>
+            </div>
+            <div>
+                <p>
+                    A central hub for digital space in the city
+                </p>
+            </div>
+            <button>
+                Our Sponsors
+            </button>
+            <Button>
+                Sign Up for Volunterer 
+            </Button>
+        </div>
+    )
+}
+
+export default BannerC
