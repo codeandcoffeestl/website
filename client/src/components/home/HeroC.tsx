b20f2a11f62a445bbe7f7d0e8effb83266883c8b
itsHackinTime <109614480+itsHackinTime@users.noreply.github.com>
2024-03-10 21:23:03 -0500

    hero content layed out

--- client/src/components/home/HeroC.tsx
+++ client/src/components/home/HeroC.tsx
@@ -3,23 +3,27 @@ import { Button } from "primereact/button"
 const HeroC = () => {
     return (
 
-        <div>
-            <div>
-                <p>
-                    A Meetup community for developers to grow and make friends  - St Louis
-                </p>
+        <div className="p-hero bg-color-black">
+            <div className="" >    
+                <section className="">
+                    <p className="p-hero-header text-6xl">
+                        A Meetup community <br/>
+                        for developers to grow <br/>
+                        and make friends  - St Louis
+                    </p>
+                    <p className="p-hero-calltoaction text-SecondaryColor">
+                        A central hub for digital space in the city
+                    </p>
+                    <div className="min-w-full flex ">
+                    <Button>
+                        Our Sponsors
+                    </Button>
+                    <Button>
+                        Sign Up for Volunterer 
+                    </Button>
+                    </div>
+                </section>
             </div>
-            <div>
-                <p>
-                    A central hub for digital space in the city
-                </p>
-            </div>
-            <Button>
-                Our Sponsors
-            </Button>
-            <Button>
-                Sign Up for Volunterer 
-            </Button>
         </div>
     )
 }
