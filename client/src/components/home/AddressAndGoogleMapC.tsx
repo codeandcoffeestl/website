8ebbe075ee675896d00c39f1cc9efd0f56cdfb9c
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-03-11 18:14:39 -0500

    Adjust some styling

--- client/src/components/home/AddressAndGoogleMapC.tsx
+++ client/src/components/home/AddressAndGoogleMapC.tsx
@@ -6,31 +6,61 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 const AddressAndGoogleMapC = () => {
 
     return (
-        <section className="bg-SecondaryColor flex">
-            <Card className="bg-GAccentColor text-PrimaryColor p-4 m-10 w-[568px] h-[363px]">
-                <div className="flex items-center justify-center pb-3">
-                    <FontAwesomeIcon icon={faLocationDot} className="fa-2xl pr-2 text-OHighlightColor" />
-                    <p className="text-[40px] text-weight-bold ">TechArtista UCity</p>
+        <section className="bg-SecondaryColor 
+                            flex 
+                            flex-col 
+                            lg:flex-row 
+                            justify-between 
+                            items-center">
+            <Card className="bg-GAccentColor 
+                            text-PrimaryColor
+                            m-10 
+                            w-[568px] 
+                            h-[363px]">
+                <div className="flex 
+                                items-center 
+                                justify-center 
+                                pb-3">
+                    <FontAwesomeIcon icon={faLocationDot} className="text-OHighlightColor 
+                                                                        fa-2xl 
+                                                                        pr-2" />
+                    <p className="text-4xl
+                                lg:text-4xl  
+                                xl:text-4xl 
+                                font-bold "
+                    >TechArtista UCity</p>
                 </div>
-                <div className="border-b-4 border-dashed border-SecondaryColor">
-                    <p className="pb-4 text-black text-[20px]">
+                <div className="border-b-4 
+                                border-dashed 
+                                border-SecondaryColor">
+                    <p className="pb-4 
+                                text-black 
+                                text-[20px]">
                         725 Kingsland Ave Suite 100 <br />
                         University City, MO 63130
                     </p>
                 </div>
-                <p className="pt-4 text-[20px] text-black">We meet twice a month on Sunday at noon.</p>
+                <p className="pt-4 
+                            text-[20px]
+                            text-black"
+                >We meet twice a month on Sunday at noon.</p>
             </Card>
-            {/* <Card className="bg-GAccentColor p-4 m-10 w-[568px] h-[363px]"> */}
             <Map
                 defaultCenter={{ lat: 38.658276, lng: -90.309074 }}
                 defaultZoom={15}
                 gestureHandling={'cooperative'}
                 disableDefaultUI={true}
-                className="p-4 rounded-lg m-10 w-[568px] h-[363px] border-4 border-GAccentColor"
+                className="rounded-lg 
+                            m-10 
+                            min-w-[312px] 
+                            min-h-[239px] 
+                            w-[568px] 
+                            h-[363px] 
+                            border-4 
+                            border-GAccentColor whitespace-normal "
             >
                 <Marker position={{ lat: 38.658276, lng: -90.309074 }} />
             </Map>
-            {/* </Card> */}
         </section >
     )
 }
