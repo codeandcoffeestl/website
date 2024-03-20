be343f522c46e7571928c5c51bd968d1d341296b
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-03-19 20:18:08 -0500

    What I think is a cleaner way of passing props.

--- client/src/components/home/sponsorComponents/SponsorCard.tsx
+++ client/src/components/home/sponsorComponents/SponsorCard.tsx
@@ -1,19 +1,21 @@
 import { Card } from "primereact/card";
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
-export const SponsorCard = (props: {
+interface SponsorCardProps {
     sponsorUrl: string | undefined;
     sponsorLogo: string | undefined;
     sponsorDescription: string | number | boolean | ReactElement<unknown, string | JSXElementConstructor<unknown>> | Iterable<ReactNode> | ReactPortal | null | undefined;
-}) => {
+}
+
+export const SponsorCard = ({ sponsorUrl, sponsorLogo, sponsorDescription }: SponsorCardProps) => {
     return (
         <div className="card flex justify-content-center ">
             <Card className="z-10 bg-PrimaryColor shadow-lg">
-                <a className="flex flex-col items-center" href={props.sponsorUrl}>
-                    <img className="w-32 h-auto" src={"../../src/assets/" + props.sponsorLogo + ".png"} alt={props.sponsorLogo + " Logo"} />
-                    <h2 className="text-lg text-SecondaryColor mt-2">{props.sponsorLogo}</h2>
+                <a className="flex flex-col items-center" href={sponsorUrl}>
+                    <img className="w-32 h-auto" src={"../../src/assets/" + sponsorLogo + ".png"} alt={sponsorLogo + " Logo"} />
+                    <h2 className="text-lg text-SecondaryColor mt-2">{sponsorLogo}</h2>
                 </a>
-                <h4 className="p-2 text-white leading-relaxed overflow-hidden">{props.sponsorDescription}</h4>
+                <h4 className="p-2 text-white leading-relaxed overflow-hidden">{sponsorDescription}</h4>
             </Card>
         </div>
     );
