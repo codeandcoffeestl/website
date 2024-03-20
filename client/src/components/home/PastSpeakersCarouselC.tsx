33502be155432464e397016187c31fb113e11e62
itsHackinTime <109614480+itsHackinTime@users.noreply.github.com>
2024-03-20 15:57:01 -0500

    added simple styling

--- client/src/components/home/PastSpeakersCarouselC.tsx
+++ client/src/components/home/PastSpeakersCarouselC.tsx
@@ -1,6 +1,21 @@
+import { Image } from "primereact/image"
+
 const PastSpeakersCarouselC = () => {
     return (
-        <div>PastSpeakersCarouselC</div>
+        <div className="container bg-WAccentColor p-4">
+
+        <div className="grid grid-cols-4  m-4">
+          
+            <Image src="../../public/temp-hero-image.png" imageClassName="aspect-video" className="flex col-span-1 justify-center items-center max-w-33" />
+
+        
+            <Image src="../../public/temp-hero-image.png" imageClassName="aspect-video" className="flex col-span-2 justify-center items-center max-w-33 " />
+        
+        
+            <Image src="../../public/temp-hero-image.png" imageClassName="aspect-video" className="flex col-span-1 justify-center items-center max-w-33 "/>
+    
+        </div>
+        </div>
     )
 }
 
