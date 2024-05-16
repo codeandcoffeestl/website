5ce36d72cb5f827a08b12b9844cae6eb74cbde9f
MikaZ21 <zukeyamamika@gmail.com>
2024-05-16 17:33:13 -0500

    Changed some style.

--- client/src/components/home/OurSponsorsC.tsx
+++ client/src/components/home/OurSponsorsC.tsx
@@ -37,22 +37,23 @@ const OurSponsorsC = () => {
 
   return (
 
-    <div className="card relative bg-cover bg-center bg-coffeeBgImg no-repeat z-0">
-      <div className="absolute inset-0 w-full h-full bg-gray-200 bg-opacity-70 z-0"></div>
+    <div className="card relative bg-cover bg-center bg-coffeeBgImg">
+      <div className="absolute inset-0 bg-gray-200 bg-opacity-70 z-0"></div>
 
       <div className="flex flex-col md:flex-row justify-between items-center pt-10 pb-5 mb-3">
         <h3 className="z-10 flex items-center text-black text-[36px] leading-tight font-bold mb-4 md:mb-0">
           <FontAwesomeIcon icon={faHandshake} className="ml-2 md:ml-4" />
           Special Thanks to Our Sponsors
         </h3>
-        <button className="z-10 text-lg bg-OHighlightColor text-PrimaryColor shadow-md hover:bg-OHighlightColor/80 hover:border hover:border-OHighlightColor md:mt-0">
+        <button className="z-10 text-lg bg-OHighlightColor text-PrimaryColor shadow-lg hover:text-PrimaryColor hover:border hover:border-OHighlightColor hover:font-bold md:mt-0">
           Become A Sponsor
         </button>
       </div>
 
 
       <hr className="relative z-10 border-2 border-dashed border-SecondaryColor" />
-      <div className="grid grid-cols-1 md:grid-cols-2 xlg:grid-cols-4">
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
         {sponsors.map((sponsor, index) => (
           <SponsorCard
             key={index}
