2157157336a121feeb664fc7f4cf966411ec6f12
MikaZ21 <zukeyamamika@gmail.com>
2024-05-17 12:12:53 -0500

    Started AboutC component and modified default button style in index.css

--- client/src/components/home/AboutC.tsx
+++ client/src/components/home/AboutC.tsx
@@ -1,6 +1,28 @@
 const AboutC = () => {
+
     return (
-        <div id="about">AboutC</div>
+        <div id="about" className="bg-SecondaryColor p-20 flex justify-center">
+
+            <div className="bg-WAccentColor py-10 px-20 max-w-3xl shadow-xl">
+             
+                <h2 className="text-black text-3xl font-bold mb-1 text-center md:text-left">
+                    About Code & Coffee St.Louis</h2>
+
+                <hr className="border-2 border-dashed border-SecondaryColor" />
+
+                <p className="mt-5 mb-4 text-center md:text-left">
+                    Code and Coffee is an inclusive, informal co-working session for anyone in the tech space! 
+                    People of all skill level attend, and we love it that way.
+                    Many people (optionally) bring projects ot work on, and many other people (optioonally) socialize the entire time.
+                    It's completely up to you!
+                </p>
+
+                <button>
+                    Contributes List &gt;&gt;
+                </button>
+            </div>
+
+        </div>
     )
 }
 
