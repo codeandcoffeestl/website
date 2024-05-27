ed673dabe70ea3355126299e3537cbc5274cceb0
MikaZ21 <zukeyamamika@gmail.com>
2024-05-27 01:19:15 -0500

    Added 'Reach me here' link text and icon.

--- client/src/components/home/aboutComponents/RolesCard.tsx
+++ client/src/components/home/aboutComponents/RolesCard.tsx
@@ -1,3 +1,5 @@
+import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal } from "react";
 
 interface RoleCardProps {
@@ -14,10 +16,19 @@ export const RolesCard: React.FC<RoleCardProps> = ({ contributorName, roleName, 
             <div className="relative w-full rounded-lg p-4 overflow-hidden bg-SecondaryColor/30 shadow-lg">
                 <div className="absolute inset-0 top-1/5 bg-SecondaryColor/50" style={{ borderTopLeftRadius: '100%' }}></div>
                 <div className="relative z-10 flex justify-between items-center pl-5 pr-5 text-left">
-                    <a href={roleUrl} style={{ background: 'transparent' }}>
+                    <div className="flex flex-col">
                         <h2 className="text-xl text-black font-bold">{contributorName}</h2>
                         <h4 className="text-black">Role: {roleName}</h4>
-                    </a>
+
+                        <a href={roleUrl} className="text-lg cursor-pointer flex items-center hover:text-shadow-none hover:font-bold" style={{ background: 'transparent' }}>
+                            <FontAwesomeIcon icon={faUserGroup} className="text-OHighlightColor fa-md pr-2" />
+                            <h4 style={{ textShadow: '2px 2px 2px rgba(0,0,0,0.2)', boxShadow: 'none'}} 
+                              onMouseEnter={(e) => e.target.style.textShadow = 'none'}
+                              onMouseLeave={(e) => e.target.style.textShadow = '2px 2px 2px rgba(0,0,0,0.3)'}
+                            >
+                                Reach me here</h4>                           
+                        </a>
+                    </div>
                     <img className="w-25 h-25 bg-SecondaryColor rounded-full object-cover" src={"../../src/assets/" + roleImage + ".png"} alt={roleImage + " roleImage"} />
                 </div>
                 <div className="relative z-10 mt-4 pl-5 md:pb-3 text-left">
