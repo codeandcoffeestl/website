4c337026a81b98f9503308bdb1a3c5569396962f
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-06-23 21:47:50 -0500

    Updated Cors for s3 deploy

--- lambda/lambda/src/lambda/Program.cs
+++ lambda/lambda/src/lambda/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy  =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173","http://cc-dev-demo.s3-website-us-east-1.amazonaws.com")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
