675e08fb2f9c706172c1b19b7172adcf5475b53a
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-11-30 10:51:17 -0600

    TS icon changes

--- client/src/components/ScrollToTopButton.tsx
+++ client/src/components/ScrollToTopButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
